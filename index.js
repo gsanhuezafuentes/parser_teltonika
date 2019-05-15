6d16dfc9618a4dad733364b50e28fbde690e4c4b
Gabriel <gsanhueza15@alumnos.utalca.cl>
2019-05-15 21:50:02 +0000

    Add pop because information is a array

--- index.js
+++ index.js
@@ -22,13 +22,14 @@ net.createServer((connection) => {
             console.log("content----------------------");
             console.log(content);
             let information = parser_content(content);
+            console.log("--------------------------------")
             console.log(information);
 
             let latitude = Buffer.allocUnsafe(4);
             let longitude = Buffer.allocUnsafe(4);
-
-            latitude.writeUInt32BE(information.latitude);
-            longitude.writeUInt32BE(information.longitude);
+        
+            latitude.writeUInt32BE(information.pop().latitude);
+            longitude.writeUInt32BE(information.pop().longitude);
             latitude = latitude.readInt32BE()/Math.pow(10,7);
             longitude = longitude.readInt32BE()/Math.pow(10,7);
             console.log(`latitude: ${latitude} | longitude: ${longitude}`);
