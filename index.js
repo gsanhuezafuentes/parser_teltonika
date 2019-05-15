d608726e1c037c88700d39cbcd6170a75bdf05ef
Gabriel <gsanhueza15@alumnos.utalca.cl>
2019-05-15 16:11:32 +0000

    Add converter to grades

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ net.createServer((connection) => {
             console.log(content);
             let information = parser_content(content);
             console.log(information);
+
+            let latitude = Buffer.allocUnsafe(4);
+            let longitude = Buffer.allocUnsafe(4);
+
+            latitude.writeUInt32BE(information.latitude);
+            longitude.writeUInt32BE(information.longitude);
+            latitude = latitude.readInt32BE()/Math.pow(10,7);
+            longitude = longitude.readInt32BE()/Math.pow(10,7);
+            console.log(`latitude: ${latitude} | longitude: ${longitude}`);
+
             connection.write(Buffer.from("00000002", 'hex'));
         }
 
